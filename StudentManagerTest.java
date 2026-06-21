9599a38af83a614e093f4f1b80ed997b38faad27
promideb771 <promideb771@gmail.com>
2026-06-22 00:57:06 +0600

    Updated documentation

--- StudentManagerTest.java
+++ StudentManagerTest.java
@@ -26,3 +26,29 @@ public class StudentManagerTest {
         assertNotNull(student);
         assertEquals("Morsalin", student.getName());
     }
+ @Test
+    void testDeleteStudent() {
+
+        StudentManager manager = new StudentManager();
+
+        manager.addStudent(new Student(1, "Morsalin"));
+
+        assertTrue(manager.deleteStudent(1));
+    }
+
+    @Test
+    void testStudentNotFound() {
+
+        StudentManager manager = new StudentManager();
+
+        assertNull(manager.searchStudent(100));
+    }
+
+    @Test
+    void testDeleteInvalidStudent() {
+
+        StudentManager manager = new StudentManager();
+
+        assertFalse(manager.deleteStudent(200));
+    }
+}
