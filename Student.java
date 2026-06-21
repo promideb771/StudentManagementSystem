6e91183faeff0553e73773c9aa2411a4cc26bedc
promideb771 <promideb771@gmail.com>
2026-06-22 00:15:22 +0600

    Added Student class

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name;
+    }
+}
